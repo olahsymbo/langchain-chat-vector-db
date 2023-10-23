5729ef8009caf58477c1dede2e1bf179c6e1b513
Olasimbo Arigbabu <olahsymbo@gmail.com>
2023-10-23 19:32:29 +0100

    Update text path

--- core/text_service.py
+++ core/text_service.py
@@ -6,5 +6,5 @@ from core.base_service import ChatService
 class TextService(ChatService):
 
     def fetch_document(self):
-        self.saved_file_path.endswith(["doc", "docx"])
+        self.saved_file_path.endswith("txt")
         self.loaders = TextLoader(self.saved_file_path)
