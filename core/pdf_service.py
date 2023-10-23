f987c4a4606c0f79ff2a7a0e85c5c57dbe974493
Olasimbo Arigbabu <olahsymbo@gmail.com>
2023-10-23 19:31:44 +0100

    Update pdf extension

--- core/pdf_service.py
+++ core/pdf_service.py
@@ -5,5 +5,5 @@ from core.base_service import ChatService
 class PDFService(ChatService):
 
     def fetch_document(self):
-        self.saved_file_path.endswith(["doc", "docx"])
+        self.saved_file_path.endswith(["pdf"])
         self.loaders = PyPDFLoader(self.saved_file_path)
